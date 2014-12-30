31687a0d0bf4e3ea4e75c59cfabfbdec073ea4a1
Eric Templeton <erictempleton1@gmail.com>
2014-12-30 20:52:57 +0000

    added notes and initial thoughts

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -1,5 +1,10 @@
+from datetime import date, timedelta
 
+def add_gigasecond(start_date):
+    d = date(start_date).timetuple()
+    tdelta_sec = timedelta(days=d[7]).total_seconds()
+    
+    # convert 1000000000 secs to years, month, and days
+    # add to start date
 
-
-def add_gigasecond(date):
-    pass
+    
