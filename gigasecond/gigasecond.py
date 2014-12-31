4e4035d4603051a20e277538a9e7ce021c1f4c2a
Eric Templeton <erictempleton1@gmail.com>
2014-12-31 15:28:04 +0000

    use timedelta to add gigasec converted to days

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -1,10 +1,16 @@
 from datetime import date, timedelta
 
 def add_gigasecond(start_date):
-    d = date(start_date).timetuple()
-    tdelta_sec = timedelta(days=d[7]).total_seconds()
+    # mins in day 24 * 60 = 1440
+    # secs in day 1440 * 60 = 86400
     
-    # convert 1000000000 secs to years, month, and days
-    # add to start date
+    # a billion seconds is 11574 days
+    # 1000000000 / 86400 = 11574
+    gigasec_days = 11574
+    
+    # use timedelta to add days to start_date
+    gigasec_aniv = start_date + timedelta(days=gigasec_days)
+    return gigasec_aniv
+
 
     
